c39eab216e9d5a754344b687d945153b68373c71
hamed <hamed.bakhshi7809@gmail.com>
2024-01-19 17:16:29 +0100

    Cell div becomes black when hovering over it
    Added an EventListener to the cells when it first creates it.
    The EventListener turns the cell black if you "hover" it, specifically
    mouseover.

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const etchContainer = document.querySelector('div')
 etchContainer.style.display = "flex"
 
+//Creates 16 divs inside of the container
 for(let i = 0; i < 16; i++){
     etchContainer.append(document.createElement('div'))
 }
 const nodeListDiv = etchContainer.querySelectorAll('div')
 console.log(nodeListDiv)
+
+// iterates through each div inside
+// of the container and makes 16 more divs inside of it.
 
 for(let i = 0; i < nodeListDiv.length; i++){
     for(let k = 1; k < 16; k++){
@@ -13,6 +17,7 @@ for(let i = 0; i < nodeListDiv.length; i++){
         tempDIV.style.flex = 'auto'
         nodeListDiv[i].style.display = 'flex'
 
+        tempDIV.addEventListener('mouseover', () => tempDIV.style.backgroundColor = 'black')
         nodeListDiv[i].append(tempDIV)
         nodeListDiv[i].style.flex = 'auto'
         nodeListDiv[i].style.flexDirection = 'column'
