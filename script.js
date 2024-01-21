64f96ca5d4ad543a69ab30d4534aed37618ef692
hamed <hamed.bakhshi7809@gmail.com>
2024-01-21 12:12:28 +0100

    Now able to input grid size
    Made the cell creation a function where it takes in an argument
    that is used to determine how to create.
    
    Event listener  on button that ask how big the gird should be
    then it calls the function for cell creation with the determined size.

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+const body = document.querySelector('body')
 const etchContainer = document.querySelector('div')
-etchContainer.style.display = "flex"
-
+etchContainer.style.display = "flex";
+let standardSize = 16
+function createGrid(size){
 //Creates 16 divs inside of the container
-for(let i = 0; i < 16; i++){
+for(let i = 0; i < size; i++){
     etchContainer.append(document.createElement('div'))
 }
 const nodeListDiv = etchContainer.querySelectorAll('div')
@@ -12,7 +14,7 @@ console.log(nodeListDiv)
 // of the container and makes 16 more divs inside of it.
 
 for(let i = 0; i < nodeListDiv.length; i++){
-    for(let k = 1; k < 16; k++){
+    for(let k = 1; k < size; k++){
         let tempDIV = document.createElement('div')
         tempDIV.style.flex = 'auto'
         nodeListDiv[i].style.display = 'flex'
@@ -23,5 +25,19 @@ for(let i = 0; i < nodeListDiv.length; i++){
         nodeListDiv[i].style.flexDirection = 'column'
     }
 }
+}
 
+createGrid(standardSize)
+let chooseSize = document.createElement('button')
+chooseSize.textContent = 'Grid Size'
+chooseSize.addEventListener('click', () => {
+    let size = prompt('How many many cells, hight by width')
+    let nodeListDelete = etchContainer.querySelectorAll('div')
+    for(let i = 0; i < nodeListDelete.length; i++){
+        let celllist = nodeListDelete[i].querySelectorAll('div')
+        nodeListDelete[i].remove()
+    }
+    createGrid(size)
+})
+body.append(chooseSize)
 console.log(etchContainer.querySelectorAll('div'))
